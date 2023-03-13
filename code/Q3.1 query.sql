e90a64b3d7e0cff7da41c3e3cc87c428c2a3e58b
Le Cong Hau <30770825+Cong-hau@users.noreply.github.com>
2023-03-13 13:07:35 +0700

    Update Q3.1 query.sql

--- code/Q3.1 query.sql
+++ code/Q3.1 query.sql
@@ -1,14 +1,10 @@
 /****** 1) Create a query to calculate the span (in days) between the hire date of the employee with the longest tenure 
 and the hire date of the employee with the shortest tenure who is still currently employed at the company.******/
-
 WITH tenure AS (
 	Select * , DATEDIFF(day, start_date, COALESCE(end_date, GETDATE())) AS tenure
 	FROM [Ahamove].[dbo].[hr]
+	Where end_date is Null
 )
 Select  max(tenure) as the_longest_tenure,
 		min(tenure) as the_shortest_tenure
 From tenure
----Get employees are being employed---
-SELECT *, ISNULL(CONVERT(varchar(10), end_date, 120), 'Yes') as currently_employed
-FROM [Ahamove].[dbo].[hr]
-WHERE end_date is Null
